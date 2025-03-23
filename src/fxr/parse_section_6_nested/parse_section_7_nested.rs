76502829827e973a10c7866ab651248bf338f176
Colin Swenson-Healey <chozandrias76@github.com>
2025-03-23 13:44:31 -0700

    refactor: split parsing for Section7Container and its nested Section8 and Section9 entries

--- src/fxr/parse_section_6_nested/parse_section_7_nested.rs
+++ src/fxr/parse_section_6_nested/parse_section_7_nested.rs
@@ -16,7 +16,6 @@ pub struct ParsedSection7Nested<'a> {
     pub section8: Vec<ParsedSection8<'a>>,
 }
 
-/// Parses Section7Container and its nested sections
 pub fn parse_section7_nested<'a>(
     data: &'a [u8],
     container: &crate::fxr::Section7Container,
@@ -24,12 +23,172 @@ pub fn parse_section7_nested<'a>(
 ) -> anyhow::Result<ParsedSection7Nested<'a>> {
     debug!("{}: Parsing Section7Container: {:#?}", label, container);
 
+    let mut parsed_section7 = parse_section7_container(data, container, label).unwrap();
+
+    parse_section7_section8_entries(data, container, label, &mut parsed_section7).unwrap();
+
+    Ok(parsed_section7)
+}
+
+fn parse_section7_section8_entries<'a>(
+    data: &'a [u8],
+    container: &crate::fxr::Section7Container,
+    label: &str,
+    parsed_section7: &mut ParsedSection7Nested<'a>,
+) -> Result<(), anyhow::Error> {
+    if container.section8_count > 0 {
+        debug!(
+            "{}: Parsing Section8[] @ offset 0x{:08X}, count {}",
+            label, container.section8_offset, container.section8_count
+        );
+        let section8_entry: Ref<&[u8], [Section8Entry]> = parse_section_slice::<Section8Entry>(
+            data,
+            container.section8_offset,
+            container.section8_count,
+            &format!("{label}::Section8[] @ 0x{:08X}:", container.section8_offset),
+        )?;
+
+        for (i, section8_entry) in section8_entry.iter().enumerate() {
+            parse_section7_section8(data, label, i, section8_entry, parsed_section7)?;
+        }
+    };
+    Ok(())
+}
+
+fn parse_section7_section8<'a>(
+    data: &'a [u8],
+    label: &str,
+    i: usize,
+    entry: &Section8Entry,
+    parsed_section7: &mut ParsedSection7Nested<'a>,
+) -> Result<(), anyhow::Error> {
+    let mut parsed_section8 = ParsedSection8 {
+        section11: Vec::new(),
+        section9: Vec::new(),
+    };
+    parse_section8_section11_entries(data, label, i, entry, &mut parsed_section8)?;
+    parse_section9_entries(data, label, i, entry, &mut parsed_section8)?;
+    parsed_section7.section8.push(parsed_section8);
+    Ok(())
+}
+
+fn parse_section9_entries<'a>(
+    data: &'a [u8],
+    label: &str,
+    i: usize,
+    section8_entry: &Section8Entry,
+    parsed_section8: &mut ParsedSection8<'a>,
+) -> Result<(), anyhow::Error> {
+    if section8_entry.section9_count > 0 {
+        debug!(
+            "{}: Parsing Section8[{}]::Section9[] @ offset 0x{:08X}, count {}",
+            label, i, section8_entry.section9_offset, section8_entry.section9_count
+        );
+        let section9_entries: Ref<&[u8], [Section9Entry]> = parse_section_slice::<Section9Entry>(
+            data,
+            section8_entry.section9_offset,
+            section8_entry.section9_count,
+            &format!(
+                "{label}::Section8[{}]::Section9[] @ 0x{:08X}",
+                i, section8_entry.section9_offset
+            ),
+        )?;
+
+        for (j, s9_entry) in section9_entries.iter().enumerate() {
+            parse_section8_section9_entry(data, label, i, j, s9_entry, parsed_section8)?;
+        }
+    };
+    Ok(())
+}
+
+fn parse_section8_section9_entry<'a>(
+    data: &'a [u8],
+    label: &str,
+    i: usize,
+    j: usize,
+    s9_entry: &Section9Entry,
+    parsed_section8: &mut ParsedSection8<'a>,
+) -> Result<(), anyhow::Error> {
+    let mut parsed_section9 = ParsedSection9 {
+        section11: Vec::new(),
+    };
+    parse_section9_section11_entries(data, label, i, j, s9_entry, &mut parsed_section9)?;
+    parsed_section8.section9.push(parsed_section9);
+    Ok(())
+}
+
+fn parse_section9_section11_entries<'a>(
+    data: &'a [u8],
+    label: &str,
+    i: usize,
+    j: usize,
+    s9_entry: &Section9Entry,
+    parsed_section9: &mut ParsedSection9<'a>,
+) -> Result<(), anyhow::Error> {
+    if s9_entry.section11_count > 0 {
+        debug!(
+            "{}: Parsing Section8[{}]::Section9[{}]::Section11[] @ offset 0x{:08X}, count {}",
+            label, i, j, s9_entry.section11_offset, s9_entry.section11_count
+        );
+        let section11_entries: Ref<&[u8], [Section11Entry]> = parse_section_slice::<Section11Entry>(
+            data,
+            s9_entry.section11_offset,
+            s9_entry.section11_count,
+            &format!(
+                "{label}::Section8[{}]::Section9[{}]::Section11[] @ 0x{:08X}",
+                i, j, s9_entry.section11_offset
+            ),
+        )?;
+        parsed_section9.section11.push(section11_entries);
+    };
+    Ok(())
+}
+
+fn parse_section8_section11_entries<'a>(
+    data: &'a [u8],
+    label: &str,
+    i: usize,
+    section8_entry: &Section8Entry,
+    parsed_section8: &mut ParsedSection8<'a>,
+) -> Result<(), anyhow::Error> {
+    if section8_entry.section11_count > 0 {
+        debug!(
+            "{}: Parsing Section8[{}]::Section11[] @ offset 0x{:08X}, count {}",
+            label, i, section8_entry.section11_offset, section8_entry.section11_count
+        );
+        let section11_entries: Ref<&[u8], [Section11Entry]> = parse_section_slice::<Section11Entry>(
+            data,
+            section8_entry.section11_offset,
+            section8_entry.section11_count,
+            &format!(
+                "{label}::Section8[{}]::Section11[] @ 0x{:08X}",
+                i, section8_entry.section11_offset
+            ),
+        )?;
+        parsed_section8.section11.push(section11_entries);
+    };
+    Ok(())
+}
+
+fn parse_section7_container<'a>(
+    data: &'a [u8],
+    container: &crate::fxr::Section7Container,
+    label: &str,
+) -> Result<ParsedSection7Nested<'a>, anyhow::Error> {
     let mut parsed_section7 = ParsedSection7Nested {
         section11: Vec::new(),
         section8: Vec::new(),
     };
+    parse_section7_section11_entries(data, container, label, &mut parsed_section7)?;
+    Ok(parsed_section7)
+}
 
-    // Section11[] from Section7Container
+fn parse_section7_section11_entries<'a>(
+    data: &'a [u8],
+    container: &crate::fxr::Section7Container,
+    label: &str,
+    parsed_section7: &mut ParsedSection7Nested<'a>,
+) -> Result<(), anyhow::Error> {
     if container.section11_count > 0 {
         debug!(
             "{}: Parsing Section11[] @ offset 0x{:08X}, count {}",
@@ -45,92 +204,6 @@ pub fn parse_section7_nested<'a>(
             ),
         )?;
         parsed_section7.section11.push(entries);
-    }
-
-    // Section8Entry[]
-    if container.section8_count > 0 {
-        debug!(
-            "{}: Parsing Section8[] @ offset 0x{:08X}, count {}",
-            label, container.section8_offset, container.section8_count
-        );
-        let entries: Ref<&[u8], [Section8Entry]> = parse_section_slice::<Section8Entry>(
-            data,
-            container.section8_offset,
-            container.section8_count,
-            &format!("{label}::Section8[] @ 0x{:08X}:", container.section8_offset),
-        )?;
-
-        for (i, entry) in entries.iter().enumerate() {
-            let mut parsed_section8 = ParsedSection8 {
-                section11: Vec::new(),
-                section9: Vec::new(),
-            };
-
-            // Section11[] inside Section8Entry
-            if entry.section11_count > 0 {
-                debug!(
-                    "{}: Parsing Section8[{}]::Section11[] @ offset 0x{:08X}, count {}",
-                    label, i, entry.section11_offset, entry.section11_count
-                );
-                let s11: Ref<&[u8], [Section11Entry]> = parse_section_slice::<Section11Entry>(
-                    data,
-                    entry.section11_offset,
-                    entry.section11_count,
-                    &format!(
-                        "{label}::Section8[{}]::Section11[] @ 0x{:08X}",
-                        i, entry.section11_offset
-                    ),
-                )?;
-                parsed_section8.section11.push(s11);
-            }
-
-            // Section9Entry[]
-            if entry.section9_count > 0 {
-                debug!(
-                    "{}: Parsing Section8[{}]::Section9[] @ offset 0x{:08X}, count {}",
-                    label, i, entry.section9_offset, entry.section9_count
-                );
-                let s9: Ref<&[u8], [Section9Entry]> = parse_section_slice::<Section9Entry>(
-                    data,
-                    entry.section9_offset,
-                    entry.section9_count,
-                    &format!(
-                        "{label}::Section8[{}]::Section9[] @ 0x{:08X}",
-                        i, entry.section9_offset
-                    ),
-                )?;
-
-                for (j, s9_entry) in s9.iter().enumerate() {
-                    let mut parsed_section9 = ParsedSection9 {
-                        section11: Vec::new(),
-                    };
-
-                    // Optional nested Section11[]
-                    if s9_entry.section11_count > 0 {
-                        debug!(
-                            "{}: Parsing Section8[{}]::Section9[{}]::Section11[] @ offset 0x{:08X}, count {}",
-                            label, i, j, s9_entry.section11_offset, s9_entry.section11_count
-                        );
-                        let s11: Ref<&[u8], [Section11Entry]> =
-                            parse_section_slice::<Section11Entry>(
-                                data,
-                                s9_entry.section11_offset,
-                                s9_entry.section11_count,
-                                &format!(
-                                    "{label}::Section8[{}]::Section9[{}]::Section11[] @ 0x{:08X}",
-                                    i, j, s9_entry.section11_offset
-                                ),
-                            )?;
-                        parsed_section9.section11.push(s11);
-                    }
-
-                    parsed_section8.section9.push(parsed_section9);
-                }
-            }
-
-            parsed_section7.section8.push(parsed_section8);
-        }
-    }
-
-    Ok(parsed_section7)
+    };
+    Ok(())
 }
